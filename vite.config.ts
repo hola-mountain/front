ce85421cf97f3450065a696d16e9b0523107d89a
LeeJams <89899636+LeeJams@users.noreply.github.com>
2022-05-08 00:39:46 +0900

    update 빌드

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,31 @@
 import { fileURLToPath, URL } from "url";
 
-import { defineConfig } from "vite";
+// import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
-    },
-  },
-});
+export default ({ command, mode }: any) => {
+  if (command === "serve") {
+    return {
+      plugins: [vue()],
+      resolve: {
+        alias: {
+          "@": fileURLToPath(new URL("./src", import.meta.url)),
+        },
+      },
+    };
+  } else {
+    return {
+      plugins: [vue()],
+      resolve: {
+        alias: {
+          "@": fileURLToPath(new URL("./src", import.meta.url)),
+        },
+      },
+      base: "/front/",
+      build: {
+        outDir: "docs",
+      },
+    };
+  }
+};
