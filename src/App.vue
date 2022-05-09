3e365673cfe41d7a580dc0ccf3541f9b72f6ed72
LeeJams <89899636+LeeJams@users.noreply.github.com>
2022-05-09 22:40:17 +0900

    feat top btn 추가

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,23 @@
   <AppHeaderVue />
   <RouterView />
   <AppFooterVue />
+  <q-btn
+    round
+    color="green-12"
+    icon="keyboard_arrow_up"
+    class="top-btn"
+    @click="scrollUp"
+  />
 </template>
 <script setup lang="ts">
 import AppHeaderVue from "./components/AppHeader.vue";
 import AppFooterVue from "./components/AppFooter.vue";
+
+const scrollUp = () => {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
+};
 </script>
